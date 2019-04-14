f9f562d073f7f9b7e40a1d4b32c517042bebbcbf
bahaa mohamed <bahaa768@gmail.com>
2019-04-14 16:52:30 +0300

    .

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('./db/mongoose');
+const cors = require("cors");
 const userRouter = require('./routers/userRouters');
 const supplierRouter = require('./routers/supplierRouters');
 const contractRouter = require('./routers/contractRouters');
@@ -10,7 +11,7 @@ const extensionRouter = require('./routers/extensionRouters');
 const amendmentRouter = require('./routers/amendmentRouters');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(supplierRouter);
